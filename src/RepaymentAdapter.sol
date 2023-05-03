f1e26d5ef901191e63f4f9c3af5747d9bec5b382
xiaohuo <xiaohuo@voyage.finance>
2023-05-03 16:24:06 +0700

    fix: remove approve and revert function

--- src/RepaymentAdapter.sol
+++ src/RepaymentAdapter.sol
@@ -13,53 +13,10 @@ contract RepaymentAdapter is IRepaymentAdapter {
     // 3. NFTFi
     mapping(address => int256) public permitLoanContract;
 
-    function approve(address currency, uint256 amount) public {
-        if (!isContract(currency)) {
-            revert InvalidCurrencyAddress();
-        }
-
-        if (IERC20(currency).balanceOf(msg.sender) < amount) {
-            revert InSufficientBalance();
-        }
-
-        IERC20(currency).approve(address(this), amount);
-    }
-
     constructor() {
         permitLoanContract[0x70b97A0da65C15dfb0FFA02aEE6FA36e507C2762] = 1;
         permitLoanContract[0xFa4D5258804D7723eb6A934c11b1bd423bC31623] = 2;
         permitLoanContract[0xE52Cec0E90115AbeB3304BaA36bc2655731f7934] = 3;
-    }
-
-    function batchApprove(address[] calldata currency, uint256[] calldata amount) public {
-        if (currency.length != amount.length) {
-            revert InvalidParameters();
-        }
-        for (uint256 i = 0; i < currency.length;) {
-            approve(currency[i], amount[i]);
-            unchecked {
-                ++i;
-            }
-        }
-    }
-
-    function revoke(address currency) public {
-        if (!isContract(currency)) {
-            revert InvalidCurrencyAddress();
-        }
-        if (IERC20(currency).allowance(msg.sender, address(this)) == 0) {
-            revert InvalidAllowance();
-        }
-        IERC20(currency).approve(address(this), 0);
-    }
-
-    function batchRevoke(address[] calldata currency) public {
-        for (uint256 i = 0; i < currency.length;) {
-            revoke(currency[i]);
-            unchecked {
-                ++i;
-            }
-        }
     }
 
     function batchRepayment(Repayment[] calldata repayment) public {
